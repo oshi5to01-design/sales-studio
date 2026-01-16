e69dc39a4b6e56890c6d1cdcdfb518c7830bb2e7
oshi5to01-design <246975035+oshi5to01-design@users.noreply.github.com>
2026-01-17 00:39:35 +0900

    画像処理APIの実装

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+import io
+
+from fastapi import FastAPI, File, Response, UploadFile
+from PIL import Image
 
 app = FastAPI()
 
@@ -6,3 +9,16 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"massage": "Hello Mercari Studio!"}
+
+
+@app.post("/process-image")
+async def process_image(file: UploadFile = File(...)):
+    # 画像を開く
+    input_image = Image.open(file.file)
+
+    # そのまま保存用データ(バイナリ)に変換する
+    img_byte_arr = io.BytesIO()
+    input_image.save(img_byte_arr, format="PNG")
+
+    # 画像として送り返す
+    return Response(content=img_byte_arr.getvalue(), media_type="image/png")
